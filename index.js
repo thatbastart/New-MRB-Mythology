feff114b7b0c60c8be46dcb9b2a680ef087c9bb3
Tilmann <tilmann.moeller@gmx.de>
2020-12-21 12:44:50 +0100

    code cleanup

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-// initialize map
+// LEAFLET MAP BASICS -----------------------------
+// maxBounds
 let southWest = L.latLng(25, -118),
     northEast = L.latLng(54, -74),
     mybounds = L.latLngBounds(southWest, northEast);
+// initialize map
 let map = L.map('map', {maxBounds: mybounds, minZoom: 10, maxZoom: 14, rotate: true, zoomControl: false});
-map.setView([43.18, -91.15], 12);
+map.setView([43.18, -91.15], 12); // map center
 
 // custom tiles
 L.tileLayer('/Tiles/{z}/{x}/{y}.png', {
-    'attribution':  'Map Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    'attribution':  'Map Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>', // panel bottomright
     'useCache': true
 }).addTo(map);
 
-// custom controls
-// control panel zoom, rotation, edit mode
+
+
+// OVERLAYS -----------------------------
+// panel topleft: zoom, rotation, edit mode, overview
 L.control.custom({
     position: "topleft",
     content: "<table><tr>" +
@@ -30,128 +34,19 @@ L.control.custom({
     },
 }).addTo(map);
 
-// title
+// panel topright: title
 L.control.custom({
     position: "topright",
     content: "<h1>The New Mississippi River Basin Mythology</h1>",
 }).addTo(map);        
         
-// Lat and Lng at cursor
+// panel bottomleft: Lat and Lng at cursor
 L.control.mousePosition({prefix: "Lat ", separator: "\nLng ", numDigits: 2}).addTo(map);
 
-// intialize marker icons; green=community; blue=editorial; red=position
-let greenIcon = L.icon({
-    iconUrl: 'marker_green.png',
-    iconSize:     [30, 30], 
-    iconAnchor:   [15, 15], 
-    popupAnchor:  [-500, 15] 
-});
-let redIcon = L.icon({
-    iconUrl: 'marker_red.png',
-    iconSize:     [16, 16], 
-    iconAnchor:   [8, 8], 
-});
 
-// reset view when marker is selected
-let curr_pu=undefined;
-let pu_flag=false;
-map.on('popupopen', function(e) {
-    setAnchor();
-    curr_pu = e.popup._source;
-    pu_flag=true;
-});
 
-map.on("popupclose", function(e) {
-    pu_flag=false;
-    if(document.getElementById("pu_title")!=null && document.getElementById("pu_content")!=null){
-        if(document.getElementById("pu_title").value=="" && document.getElementById("pu_content").value==""){
-            map.removeLayer(marker);
-            arr_marker.pop();
-        } else {
-            if (confirm("The content of this marker is not submitted yet. Are you sure you want to close it? All content will be lost!")){
-                map.removeLayer(marker);
-                arr_marker.pop();
-            } else {
-                add_marker(marker._latlng,document.getElementById("pu_title").value,document.getElementById("pu_content").value);
-            }
-        }
-    }
-});
-
-let arr_marker = [];
-
-// Fetch all notes from the database and add them to the map.
-fetch("/api/get_notes", {
-    cache: "no-cache"
-})
-.then(response => response.json())
-.then(json => json.map(note => {
-    let marker = new L.marker({ lat: note.lat, lng: note.lon }, {icon: greenIcon}).addTo(map);
-    let title = note.versions[note.versions.length-1].title;
-    let text =  note.versions[note.versions.length-1].text;
-    marker.bindPopup("<h1>" + title + "</h1><br>" + text);
-    marker.noteVersions = note.versions;
-    arr_marker.push(marker);
-}))
-.catch(err => console.log(err));
-
-// popup (pu) submit function
-function pu_submit(){
-    let title = L.DomUtil.get("pu_title").value;
-    let text = L.DomUtil.get("pu_content").value;
-    let latlng = arr_marker[arr_marker.length-1]._latlng;
-    let converter = new showdown.Converter({extensions: ["htmlescape"]});
-    let content= converter.makeHtml(text);
-    arr_marker[arr_marker.length-1].bindPopup("<h1>" + title + "</h1><br><div id='pu_content_ld'>" + content +"</div>");
-
-    // push the created note to the database.
-    fetch("/api/add_note", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      cache: "no-cache",
-      // The backend only accepts this very request structure. Ping Kerstin for more fields etc.
-      body: JSON.stringify( {
-        versions: [ {
-          title: title,
-          text: text
-        } ],
-        lat: latlng.lat,
-        lon: latlng.lng,
-        kind: ""
-      } )
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
-}
-
-// adding marker
-let marker=undefined;
-function add_marker(pos,title,content){
-    marker = new L.marker(pos, {icon: greenIcon}).addTo(map);
-    
-    title = (typeof title !== 'undefined') ?  title : "";
-    content = (typeof content !== 'undefined') ?  content : "";
-    marker.bindPopup("<textarea id='pu_title' rows='1' style='text-align: center'>" + title + "</textarea><br><br>" +
-                    "<textarea id='pu_content' rows='30'>" + content + "</textarea><br><br>" +
-                    "<div class='tooltip'>You can use Markdown to format the text." + 
-                    "<span class='tooltiptext'>Heading 1: # <br>Heading 2: ## <br>Italics: *Text* <br>Bold: **Text** <br>Blockquote: < Text <br>Horizontal Line: --- <br>Links: [Text](URL) <br>Paragraph: Empty Line</span></div><br><br>" +
-                    "<button type='button' style='width:80px; height: 20px;' onclick='pu_submit()'>Submit</button>")
-    
-    arr_marker.push(marker);
-    view_reset(pos.lat,pos.lng);
-    let rot=document.getElementById("ctrl_rotate").value;
-    setAnchor();
-    marker.openPopup();
-}
-
-map.on('click', function(e){
-    if(document.getElementById("ctrl_edit").checked==true && document.getElementById("ctrl_ov").checked==false){
-        add_marker(e.latlng);
-    }
-});
-
-// set rotation on slider change (continous)
+// OVERLAY FUNCTIONS -----------------------------
+// rotation slider
 window.onload = function() {
     let slider = document.getElementById("ctrl_rotate");
     slider.addEventListener('input', function () {
@@ -163,49 +58,11 @@ window.onload = function() {
     });
 }
 
-// focus on marker; coord offset depending on zoom level
-function change_view(lt,ln){
-    let offset=0;
-    switch(map.getZoom()){
-        case 10:
-            offset=0.275;
-            break;
-        case 11:
-            offset=0.15;
-            break;
-        case 12:
-            offset=0.075;
-            break;
-        case 13:
-            offset=0.04;
-            break;
-        case 14:
-            offset=0.015;
-            break;
-    }
-    map.setView([lt+offset, ln-offset]);
-}
-
-// reset rotation and focus
-function view_reset(lt,ln){
-    map.setBearing(0);
-    document.getElementById("ctrl_rotate").value=0;
-    document.getElementById("ctrl_rotate").style.background = 'linear-gradient(to right, #00a104 0%, #00a104 0%, #fff 0%, white 100%)';
-    change_view(lt,ln); 
-}
-
 // shading the rotation slider bar
 document.getElementById("ctrl_rotate").oninput = function() {
     this.style.background = 'linear-gradient(to right, #00a104 0%, #00a104 ' + this.value/3.6 + '%, #fff ' + this.value/3.6 + '%, white 100%)'
 };
 
-
-// edit
-function edit(){
-    if(document.getElementById("ctrl_ov").checked==true){
-        document.getElementById("ctrl_edit").checked=false;
-    }
-}
 
 // overview
 let curr_view=[0.0,0.0,0]
@@ -238,6 +95,163 @@ function overview(){
     }
 }
 
+// set edit depending on overview
+function edit(){
+    if(document.getElementById("ctrl_ov").checked==true){
+        document.getElementById("ctrl_edit").checked=false;
+    }
+}
+
+
+
+// MARKER -----------------------------
+
+// intialize marker icons; green=community; blue=editorial; red=position
+let greenIcon = L.icon({
+    iconUrl: 'marker_green.png',
+    iconSize:     [30, 30], 
+    iconAnchor:   [15, 15], 
+    popupAnchor:  [-500, 15] 
+});
+
+let redIcon = L.icon({
+    iconUrl: 'marker_red.png',
+    iconSize:     [16, 16], 
+    iconAnchor:   [8, 8], 
+});
+
+// POPUP OPEN
+let curr_pu=undefined;
+let pu_flag=false;
+map.on('popupopen', function(e) {
+    setAnchor();
+    curr_pu = e.popup._source;
+    pu_flag=true;
+});
+
+// POPUP CLOSE
+map.on("popupclose", function(e) {
+    pu_flag=false;
+    if(document.getElementById("pu_title")!=null && document.getElementById("pu_content")!=null){
+        if(document.getElementById("pu_title").value=="" && document.getElementById("pu_content").value==""){
+            map.removeLayer(marker);
+            arr_marker.pop();
+        } else {
+            if (confirm("The content of this marker is not submitted yet. Are you sure you want to close it? All content will be lost!")){
+                map.removeLayer(marker);
+                arr_marker.pop();
+            } else {
+                add_marker(marker._latlng,document.getElementById("pu_title").value,document.getElementById("pu_content").value);
+            }
+        }
+    }
+});
+
+let arr_marker = [];
+
+// GET NOTES - Fetch all notes from the database and add them to the map.
+fetch("/api/get_notes", {
+    cache: "no-cache"
+})
+.then(response => response.json())
+.then(json => json.map(note => {
+    let marker = new L.marker({ lat: note.lat, lng: note.lon }, {icon: greenIcon}).addTo(map);
+    let title = note.versions[note.versions.length-1].title;
+    let content =  note.versions[note.versions.length-1].text;
+    marker.bindPopup("<h1>" + title + "</h1><br><div id='pu_content_ld'>" + content +"</div>");
+    marker.noteVersions = note.versions;
+    arr_marker.push(marker);
+}))
+.catch(err => console.log(err));
+
+// ADD NOTES - popup submit function
+function pu_submit(){
+    let title = L.DomUtil.get("pu_title").value;
+    let text = L.DomUtil.get("pu_content").value;
+    let latlng = arr_marker[arr_marker.length-1]._latlng;
+    let converter = new showdown.Converter({extensions: ["htmlescape"]});
+    let content= converter.makeHtml(text);
+    arr_marker[arr_marker.length-1].bindPopup("<h1>" + title + "</h1><br><div id='pu_content_ld'>" + content +"</div>");
+
+    // push the created note to the database.
+    fetch("/api/add_note", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      cache: "no-cache",
+      // The backend only accepts this very request structure. Ping Kerstin for more fields etc.
+      body: JSON.stringify( {
+        versions: [ {
+          title: title,
+          text: content
+        } ],
+        lat: latlng.lat,
+        lon: latlng.lng,
+        kind: ""
+      } )
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+}
+
+// ADDING MARKER FUNCTION
+let marker=undefined;
+function add_marker(pos,title,content){
+    marker = new L.marker(pos, {icon: greenIcon}).addTo(map);
+    
+    title = (typeof title !== 'undefined') ?  title : "";
+    content = (typeof content !== 'undefined') ?  content : "";
+    marker.bindPopup("<textarea id='pu_title' rows='1' style='text-align: center'>" + title + "</textarea><br><br>" +
+                    "<textarea id='pu_content' rows='30'>" + content + "</textarea><br><br>" +
+                    "<div class='tooltip'>You can use Markdown to format the text." + 
+                    "<span class='tooltiptext'>Heading 1: # <br>Heading 2: ## <br>Italics: *Text* <br>Bold: **Text** <br>Blockquote: < Text <br>Horizontal Line: --- <br>Links: [Text](URL) <br>Paragraph: Empty Line</span></div><br><br>" +
+                    "<button type='button' style='width:80px; height: 20px;' onclick='pu_submit()'>Submit</button>")
+    
+    arr_marker.push(marker);
+    view_reset(pos.lat,pos.lng);
+    setAnchor();
+    marker.openPopup();
+}
+
+// onClick Event -> adding marker
+map.on('click', function(e){
+    if(document.getElementById("ctrl_edit").checked==true && document.getElementById("ctrl_ov").checked==false){
+        add_marker(e.latlng);
+    }
+});
+
+
+// UTILITY FUNCTIONS -----------------------------
+// focus on marker; coord offset depending on zoom level
+function change_view(lt,ln){
+    let offset=0;
+    switch(map.getZoom()){
+        case 10:
+            offset=0.275;
+            break;
+        case 11:
+            offset=0.15;
+            break;
+        case 12:
+            offset=0.075;
+            break;
+        case 13:
+            offset=0.04;
+            break;
+        case 14:
+            offset=0.015;
+            break;
+    }
+    map.setView([lt+offset, ln-offset]);
+}
+
+// reset rotation and focus
+function view_reset(lt,ln){
+    map.setBearing(0);
+    document.getElementById("ctrl_rotate").value=0;
+    document.getElementById("ctrl_rotate").style.background = 'linear-gradient(to right, #00a104 0%, #00a104 0%, #fff 0%, white 100%)';
+    change_view(lt,ln); 
+}
 
 // redefine icon size
 function setIcon(ico,size){
@@ -262,7 +276,7 @@ let rot_matrix={0: [-500,15],
     300: [-378,-495],
     330: [-505,-255],
     360: [-500,15]} 
-    
+
 // redefine pu anchor
 function setAnchor(){
     let rot=document.getElementById("ctrl_rotate").value;
